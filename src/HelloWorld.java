a553872b918b6d4936f08dedd2221fb8ea1b1df0
teasingmonkey <nikieme3@gmail.com>
2012-10-09 10:33:07 -0300

    Update src/HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 	
   public String hello()
   {
-	 System.out.println("This is Sysc3110 test code.")
+	 System.out.println("This is Sysc3110 test code.");
 	 return "Welcome to the new era";
   }
 
